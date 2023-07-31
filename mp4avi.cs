a4e0f512c5fbd0a4d0bc98135d1ad883ec4a95d9
Muhammad Ahmad <muhammad.ahmad@alliedc.com>
2023-08-01 01:34:10 +0500

    Initial commit

---
+++ mp4avi.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Storage.Blob;
+using Azure.Core;
+using BlobProperties = Azure.Storage.Blobs.Models.BlobProperties;
+
+namespace converter_service_w_dotnet
+{
+    public class Mp4avi
+    {
+        public static readonly string incomingContainerName = "incoming-mp4-n-avi";
+        public static readonly string processedContainerName = "processed-videos";
+        public static readonly string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+
+        [FunctionName("Mp4avi")]
+        public static async Task Run(
+          [BlobTrigger("incoming-mp4-n-avi/{name}", Connection = "AzureWebJobsStorage")] Stream input,
+          string name,
+          ILogger log)
+        {
+            BlobContainerClient containerClient = new BlobContainerClient(connectionString, incomingContainerName);
+            BlobClient blobClient = containerClient.GetBlobClient(name);
+            BlobProperties blobProperties = await blobClient.GetPropertiesAsync();
+
+            string fromFormat = blobProperties.Metadata["from"];
+            string toFormat = blobProperties.Metadata["to"];
+            string userId = blobProperties.Metadata["uuid"];
+
+            //string fromFormat = ".mp4";
+            //string toFormat = ".mkv";
+            //string userId = "jackass";
+
+            string inputExt = Path.GetExtension(name).ToLower();
+            string outputName = userId + "-" + Guid.NewGuid().ToString() + toFormat;
+            string inputFile = Path.GetTempFileName() + inputExt;
+
+            using (FileStream fs = new FileStream(inputFile, FileMode.Create, FileAccess.Write))
+            {
+                input.CopyTo(fs);
+            }
+
+            string outputFile = Path.GetTempFileName() + toFormat;
+            var process = new Process();
+
+            //process.StartInfo.FileName = "ffmpeg";
+            process.StartInfo.FileName = "C:\\home\\ffmpeg\\ffmpeg.exe";
+
+
+            if (fromFormat == ".mp4")
+                process.StartInfo.Arguments = $"-i {inputFile} {outputFile}";
+            if (fromFormat == ".avi")
+                process.StartInfo.Arguments = $"-i {inputFile} -c copy {outputFile}";
+
+            process.Start();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                log.LogError("FFmpeg conversion failed");
+                return;
+            }
+
+            using (FileStream output = File.OpenRead(outputFile))
+            {
+                await UploadBlobAsync(output, outputName, log);
+            }
+
+            File.Delete(inputFile);
+            File.Delete(outputFile);
+        }
+        private static async Task UploadBlobAsync(Stream input, string name, ILogger log)
+        {
+            try
+            {
+                BlobContainerClient container = new BlobContainerClient(connectionString, processedContainerName);
+                BlobClient blob = container.GetBlobClient(name);
+                log.LogInformation($"Uploading to {name}");
+                await blob.UploadAsync(input);
+                log.LogInformation("Upload succeeded");
+            }
+            catch (Exception ex)
+            {
+                log.LogError("Upload failed", ex);
+                throw;
+            }
+        }
+    }
+
+}
